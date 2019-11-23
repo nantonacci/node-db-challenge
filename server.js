299741d0344b20af3eb6f8bcad54a425f21d6d34
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-11-23 09:27:06 -0800

    Basic file structure set up. Migration built.

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const db = require("./data/db-config.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+// server.get methods go here
+
+module.exports = server;
